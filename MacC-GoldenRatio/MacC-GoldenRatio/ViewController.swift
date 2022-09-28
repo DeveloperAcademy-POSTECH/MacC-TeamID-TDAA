2aa9c1006d987a604d73e50740b033c5654ccbe1
woo0 <57060443+woo0dev@users.noreply.github.com>
2022-09-28 20:20:18 +0900

    create project

---
+++ MacC-GoldenRatio/MacC-GoldenRatio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MacC-GoldenRatio
+//
+//  Created by woo0 on 2022/09/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+

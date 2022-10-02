7e35ad3af3db480a508487cf3de1af95581d8bac
Dongkyu Kim <dk143496@gmail.com>
2022-10-02 12:47:43 +0900

    [Feat] #9 로그아웃 / 회원탈퇴 테스트용 vc 추가

--- MacC-GoldenRatio/MacC-GoldenRatio/Sources/ViewControllers/SignIn/SignInViewController.swift
+++ MacC-GoldenRatio/MacC-GoldenRatio/Sources/ViewControllers/SignIn/SignInViewController.swift
@@ -7,8 +7,11 @@
 
 import UIKit
 import SnapKit
+import AuthenticationServices
 
 class SignInViewController: UIViewController {
+    
+    let myImageView:UIImageView = UIImageView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,10 +28,11 @@ class SignInViewController: UIViewController {
         button.backgroundColor = .black
         button.tintColor = .white
         button.setImage(UIImage(systemName: "applelogo"), for: .normal)
-        button.setPreferredSymbolConfiguration(.init(pointSize: 28, weight: .regular, scale: .default), forImageIn: .normal)
-        button.setTitle(" Apple ID로 로그인", for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 28)
+        button.setPreferredSymbolConfiguration(.init(pointSize: 21.4),forImageIn: .normal)
+        button.setTitle("  Apple로 로그인", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 21.4)
         button.addTarget(self, action: #selector(appleLoginButtonPressed), for: .touchUpInside)
+        button.layer.cornerRadius = 4
         return button
     }()
     
@@ -36,16 +40,26 @@ class SignInViewController: UIViewController {
     
     @objc private func appleLoginButtonPressed() {
         print("Sign In completed with Apple ID")
+        
+        showTestView()
+    }
+    
+    func showTestView() {
+        let signInTestVC = SignInTestViewController()
+        self.navigationController?.pushViewController(signInTestVC, animated: true)
     }
     
     // MARK: - setup
     
     private func setup() {
         view.addSubview(appleLoginButton)
+        
         appleLoginButton.snp.makeConstraints {
-            $0.size.equalTo(CGSize(width: 300, height: 50))
+            $0.size.equalTo(CGSize(width: 268, height: 50))
             $0.centerX.equalToSuperview()
-            $0.centerY.equalToSuperview()
+            $0.bottom.equalToSuperview().inset(80)
         }
+
     }
+    
 }
